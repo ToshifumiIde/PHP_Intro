c59b3825cab4ee9c205cf82a2126001da8e81ca0
ToshifumiIde <toshifumiide198765@gmail.com>
2021-03-17 22:44:12 +0900

    training if syntax

--- sample10.php
+++ sample10.php
@@ -18,6 +18,26 @@
 <main>
 <h2>Practice</h2>
 <pre>
+<?php
+  date_default_timezone_set("Asia/Tokyo");
+  // print(date("G"));
+  // print("確認");
+  if(date("G") < 9 || date("G") >= 22){
+    print("※現在受付時間外です。" . "\n");
+  }else{
+  print("ようこそ！" . "\n");
+
+  }
+
+  $x = "あいうえお";
+  //文字列は空の場合false、数字は0がfalse、0以外はtrue
+  // if($x !==""){
+  if(!$x){
+    print("xには文字が入力されていません。" . "\n");
+  }else{
+    print("xには文字が入力されています。" . "\n");
+  }
+?>
 <!-- ここにプログラムを記述します -->
 </pre>
 </main>
