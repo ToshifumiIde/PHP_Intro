3f315b69d6b028a9780d57eb8a5ad2bcd5671da0
ToshifumiIde <toshifumiide198765@gmail.com>
2021-04-19 14:02:31 +0900

    create header function

---
+++ sample22.php
@@ -0,0 +1,8 @@
+<?php
+
+header("Location: https://h2o-space.com" );
+exit();
+
+// header() functionを用いるとブラウザに対しheaderを送信する。表示するジャンプする、別の動きをするのか、様々な処理を実行できる。
+//Locationは「別の場所に移動する」という命令となる。
+// exit()functionは処理を強制終了するfunction
